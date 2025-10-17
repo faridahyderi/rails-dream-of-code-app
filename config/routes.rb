1531127e79d7dc1928b1d4dc921ea33d74b52731
Farida <faridahyderi@gmail.com>
2025-10-17 16:56:37 -0400

    Assignment 14

--- config/routes.rb
+++ config/routes.rb
@@ -36,4 +36,12 @@ Rails.application.routes.draw do
       get '/courses/:course_id/enrollments', to: 'enrollments#index'
     end
   end
+
+  namespace :api do
+    namespace :v1 do
+      get '/courses', to: 'courses#index'
+      get '/courses/:course_id/enrollments', to: 'enrollments#index'
+      post '/students', to: 'students#create'
+    end
+  end
 end
